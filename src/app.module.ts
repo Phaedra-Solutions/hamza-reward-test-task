fb993121b0c4b452f39bb5d16097f44aa59f12ff
hamza <mujtaba@phaedrasolutions.com>
2022-07-24 03:39:25 +0500

    - added transaction module with CRUD operations

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TransactionModule } from './transaction/transaction.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TransactionModule,
   ],
   controllers: [AppController],
   providers: [AppService],
